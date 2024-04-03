89f290f090fb4794f2d6599124af7897d8483674
PC Magik - Home Lab <biuro@mateuszpiekut.pl>
2024-04-03 14:29:08 +0200

    Add update_protection_commands.sh script

---
+++ update_protection_commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+proxmox-backup-client snapshot protected update ct/100/2024-03-31T19:00:01Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-03-31T01:00:02Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-04-01T22:00:00Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-04-02T19:00:02Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-04-01T01:00:01Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-03-31T22:00:08Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-03-30T02:00:04Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2023-11-25T11:39:10Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-04-02T01:00:03Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2023-11-25T14:09:28Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-04-01T19:00:00Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-03-30T23:00:02Z false --repository root@pam@pbs:BACKUP-PBS
+proxmox-backup-client snapshot protected update ct/100/2024-03-30T20:00:01Z false --repository root@pam@pbs:BACKUP-PBS
